8317ada3da717689eb955c84336fb80828463b51
Nkele22 <nkelemafemo@gmail.com>
2023-07-18 15:27:43 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Main function
  *
- * Return : o on success
+ * return: o on success
  */
 int main(void)
 {
@@ -12,6 +12,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
 	return (0);
 }
